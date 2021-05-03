27302f7488324b7db694a66db118211c5c259140
Vismmax <vismmax@gmail.com>
2021-05-03 21:08:29 +0300

    init: add empty routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './components/App';
+import { store } from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
