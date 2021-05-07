92839773e797d31c7ff2aef0b78bb26bf8cc4562
Vismmax <vismmax@gmail.com>
2021-05-07 09:37:31 +0300

    feat: add routes

---
+++ frontend/src/common/enums/RoutesEnum.ts
@@ -0,0 +1,8 @@
+export enum Routes {
+  catalog = 'catalog',
+  video = 'video',
+  login = 'login',
+  registration = 'registration',
+  subscription = 'subscription',
+  user = 'user',
+}
--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Routes } from '../../common/enums/RoutesEnum';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Switch>
+        {/*<Route path={Routes.catalog} component={CatalogPage} />*/}
+        {/*<Route path={Routes.video} component={VideoPage} />*/}
+        {/*<Route path={Routes.login} component={LoginPage} />*/}
+        {/*<Route path={Routes.registration} component={RegistrationPage} />*/}
+        {/*<Route path={Routes.user} component={UserPage} />*/}
+        {/*<Route path={Routes.subscription} component={SubscriptionPage} />*/}
+        {/*<Redirect from='/' to={Routes.catalog} />*/}
+      </Switch>
+    </>
+  );
 }
 
 export default App;
