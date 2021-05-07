105484681d0ad78044735a745562987ae641c065
Vismmax <vismmax@gmail.com>
2021-05-07 10:26:43 +0300

    feat: add bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import { store } from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
