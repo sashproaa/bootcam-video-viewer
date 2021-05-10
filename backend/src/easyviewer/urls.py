06febaf88675965e1d9be7fa87375142836efc93
znnznn <znnznn@ukr.net>
2021-05-10 14:18:06 +0300

    Користувацькі URLs

--- backend/src/easyviewer/urls.py
+++ backend/src/easyviewer/urls.py
@@ -1,21 +1,13 @@
-"""walletsrank URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('project/create/', ProjectCreateApiView.as_view()),
+    path('project/<int:pk>', ProjectDetailApiView.as_view()),
+    path('project/list/', ProjectListApiView.as_view()),
+    path('video/list/', VideoListApiView.as_view()),
+    path('video/<int:pk>', VideoApiView.as_view()),
+    path('user/<int:pk>', UserProfileApiView.as_view()),
+    path('user/transactions', TransactionsApiView.as_view()),
 ]
