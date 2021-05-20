40249520c008fcc6f0e2adc8d062521a4858dcb1
Vismmax <vismmax@gmail.com>
2021-05-20 13:12:19 +0300

    feat: update ui catalog

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,28 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import { Routes } from '../../common/enums/RoutesEnum';
+import cls from './style.module.css';
+import './style.css';
+import tt from '../../assets/ticket.svg';
+import { IoTicket } from 'react-icons/io5';
+import { IoLogInOutline } from 'react-icons/io5';
+import { IoLogIn } from 'react-icons/io5';
 
 export default function HeaderPage() {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
 
   useEffect(() => {}, []);
 
+  const handleAuth = () => {};
+
+  const handleSearch = (ev: { target: { value: string } }) => {
+    setTitle(ev.target.value);
+  };
+
   return (
-    <Navbar className='mb-3' bg='dark' variant='dark'>
-      <Link to={Routes.catalog}>
-        <Navbar.Brand>Logo</Navbar.Brand>
-      </Link>
-      <Form className='mr-auto ml-auto' inline>
-        <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-        <Button variant='outline-info'>Search</Button>
-      </Form>
-      <Nav>
-        <Link to={Routes.profile}>
-          <Navbar.Text>Login</Navbar.Text>
-        </Link>
-      </Nav>
-    </Navbar>
+    <div className='wrapper-head d-flex align-items-center'>
+      <div className='container'>
+        <div className='row header-row d-flex justify-content-between'>
+          <div className='col-12 header d-flex justify-content-between'>
+            <h1>
+              <Link className='logoLink' to={Routes.catalog}>
+                AWplayer
+              </Link>
+            </h1>
+
+            <div className='registr d-flex'>
+              <form className='search-form'>
+                <label htmlFor='search'>
+                  <span className='searching-mark'></span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='Поиск'
+                  id='search'
+                  value={title}
+                  onChange={handleSearch}
+                />
+              </form>
+              <Link to={Routes.subscription}>
+                {/*<button className='subscribe'>Подписаться</button>*/}
+                <button className='subscribe'>
+                  <span className='subscribe-text'>Подписаться</span>
+                  <span className='subscribe-icon'>
+                    <IoTicket size={24} />
+                  </span>
+                </button>
+              </Link>
+              <button className='enter' onClick={handleAuth}>
+                <span className='auth-text'>Войти</span>
+                <span className='auth-icon'>
+                  <IoLogInOutline size={32} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/CatalogPage/CatalogCard/index.tsx
+++ frontend/src/pages/CatalogPage/CatalogCard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Card } from 'react-bootstrap';
 import { Video } from '../../../common/interfaces/VideoInterface';
 import { Routes } from '../../../common/enums/RoutesEnum';
+import './style.css';
 
 interface Props {
   video: Video;
@@ -10,19 +10,16 @@ interface Props {
 
 export default function CatalogCard({ video }: Props) {
   return (
-    <Card style={{ width: '30rem', margin: '2rem' }}>
-      <Card.Img
-        variant='top'
-        src={video.image}
-        style={{ width: '30rem', height: '20rem' }}
-      />
-      <Card.Body>
-        <Card.Title>{video.title}</Card.Title>
-        <Card.Text>{video.description}</Card.Text>
-        <Link to={`${Routes.video}/${video.id}`}>
-          <Button variant='primary'>Go to video</Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <div className='col-12 col-md-6 col-xl-4 video-block'>
+      <Link to={`${Routes.video}/${video.id}`}>
+        <div className='block-1 video'>
+          <img className='cardImage' src={video.image} alt={video.title} />
+          <div className='info'>
+            <p>{video.description}</p>
+          </div>
+        </div>
+        <strong>{video.title}</strong>
+      </Link>
+    </div>
   );
 }
