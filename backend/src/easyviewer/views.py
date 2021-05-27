86ce02640aea8d22034b1cc237b1c237fd3eb617
znnznn <znnznn@ukr.net>
2021-05-28 02:07:26 +0300

    Зделал запись проплаченого видео и танзакций о
    проблема с записью если купил подпису нечегго впихивать в video_id плюч подчистил вьюхи и добавил кверисети по юзерам там где чужое нельзя смотреть

--- backend/src/easyviewer/views.py
+++ backend/src/easyviewer/views.py
@@ -1,10 +1,12 @@
+import json
+
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.salesforce.views import SalesforceOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 from django.db.models import Case, When, ExpressionWrapper, F, Q
 from django.utils import timezone
-from rest_framework import generics
+from rest_framework import generics, request
 from django.contrib.auth import get_user_model
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
@@ -36,24 +38,45 @@ class ProjectDetailApiView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class VideoListApiView(generics.ListAPIView):
-    queryset = Video.objects.all().annotate(paid_video=Case(
-        When(videocontent__data_end__gte=timezone.now(), then=F('videocontent__data_end')),
-        output_field=models.DateTimeField()))  # пока не смог связать с юзером (пока дает все даже чужое)
+    pagination_class = VideoPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['genre', 'title']
     serializer_class = VideoListSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    pagination_class = VideoPagination
+    temp = ""
+
+    def get(self, request, *args, **kwargs):
+
+        self.temp = request.GET.get('temp')
+        print("Get request user_id", request.user.id)
+        return super(VideoListApiView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            queryset = Video.objects.all().annotate(paid_video=Case(
+                When(Q(videocontent__data_end__gte=timezone.now()) & Q(videocontent__user_id=self.request.user.id),
+                     then=F('url')),
+                output_field=models.CharField()))
+        else:
+            queryset = Video.objects.all()
+        return queryset
+
 
 class VideoContentListApiView(generics.ListAPIView):
     queryset = VideoContent.objects.all()
     serializer_class = VideoContentDetailSerializer
-    permission_classes = (IsOwnerOrReadonly, IsAuthenticatedOrReadOnly)
+    permission_classes = (IsAuthenticated, )
 
+    def get(self, request, *args, **kwargs):
+        self.temp = request.GET.get('temp')
+        print("Get request user_id", request.user.id)
+        return super(VideoContentListApiView, self).get(request, *args, **kwargs)
 
-class VideoContentCreateApiView(generics.CreateAPIView):
-    serializer_class = VideoContentCreateSerializer
-    permission_classes = (IsOwnerOrReadonly, IsAuthenticatedOrReadOnly)
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            queryset = VideoContent.objects.filter(user_id=self.request.user.id)
+        else:
+            queryset = None
+        return queryset
 
 
 class VideoApiView(generics.RetrieveUpdateDestroyAPIView, generics.CreateAPIView):
@@ -62,13 +85,78 @@ class VideoApiView(generics.RetrieveUpdateDestroyAPIView, generics.CreateAPIView
     permission_classes = (IsOwnerOrReadonly, IsAuthenticatedOrReadOnly,)
 
 
-class TransactionsApiView(generics.ListAPIView, generics.CreateAPIView):
-    queryset = Transactions.objects.filter()
+class TransactionsListApiView(generics.ListAPIView):
+
     serializer_class = TransactionsDetailSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    temp = ""
+
+    def get(self, request, *args, **kwargs):
+
+        self.temp = request.GET.get('temp')
+        print("Get request user_id", request.user.id)
+        return super(TransactionsListApiView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            queryset = Transactions.objects.filter(user_id=self.request.user.id)
+        else:
+            queryset = None
+        return queryset
+
+
+class TransactionsApiView(generics.CreateAPIView):
+    serializer_class = MerchantFondySerializer
     permission_classes = (IsAuthenticated, IsAuthenticatedOrReadOnly)
 
-    # def create(self, request, *args, **kwargs):
-    #     pass
+    def create(self, request, *args, **kwargs):
+        post_obj = self.request.data
+        # user = self.request.user.id # берем из реквеста банка (фронт записал туда id)
+        if post_obj['order_status'] == 'approved':
+            json_description = post_obj
+            price = float(post_obj['amount']) / 100  # может есть по прощче перенести знак на два чила
+            status = 'Payed'  # post_obj['order_status']  тут у нас не совпадают чойс філди
+            title = post_obj['order_id']  # надо что то придумать может что то другое
+            created_at = timezone.now()
+            merchant_data = json.loads(post_obj['merchant_data'])[0]
+            instance_id = merchant_data['value']['id']
+            user = merchant_data['value']['userId']
+            project_id = 1  # merchant_data['value']['project_id'] Вітя должен добавить в мерчант дату
+            transactions_data = {
+                'user_id': user, 'title': title, 'stutus': status, 'price': price,
+                'project_id': project_id, 'json_description': json_description,
+                'created_at': created_at, 'videocontent': None            }
+            transaction = TransactionsDetailSerializer(data=transactions_data)
+            if transaction.is_valid():
+                transaction_obj = transaction.save()
+            else:
+                print(transaction.errors)
+                raise
+            if 'video' == merchant_data['value']['target']:
+                video = Video.objects.get(id=instance_id)
+                video_id = instance_id
+                sub = video.subscription
+                subs = VideoSubscriptions.objects.get(video=video_id)
+                subscription = subs.id
+                duration = video.duration
+            elif 'subscription' == merchant_data['value']['target']:  # тут проблема когда подписка нечего записивать в відео id
+                subscription = VideoSubscriptions.objects.get(id=instance_id)
+                duration = subscription.duration
+            data_start = timezone.now()
+            data_end = timezone.now() + duration
+            videocontent_data = {
+                'transaction_id': transaction_obj.id,
+                'data_start': data_start, 'data_end': data_end, 'user_id': user, 'video_id': video_id,
+                'video_subscription': subscription
+            }
+            videocontent = VideoContentCreateSerializer(data=videocontent_data)
+            transaction.videocontent = videocontent
+            if videocontent.is_valid():
+                videocontent.save()
+            else:
+                print(videocontent.errors)
+                raise
+            return Response(transaction.data)
 
 
 class ProjectSubscriptionsApiView(generics.RetrieveUpdateDestroyAPIView):
@@ -95,5 +183,3 @@ class FacebookLogin(SocialLoginView):
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-
-
