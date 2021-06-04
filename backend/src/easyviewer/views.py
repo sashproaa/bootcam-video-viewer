688cfce0bb18a36f4cfc009af94de24e3891b1a5
znnznn <znnznn@ukr.net>
2021-06-04 21:37:36 +0300

    добавил логику по subscription если есть подписка тогда все видео нет тогда оплаченое  видео

--- backend/src/easyviewer/views.py
+++ backend/src/easyviewer/views.py
@@ -80,10 +80,16 @@ class VideoContentListApiView(generics.ListAPIView):
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            queryset = Video.objects.filter(Q(videocontent__user_id=self.request.user.id) &
-                                            Q(videocontent__data_end__gte=timezone.now())
-                                            ).annotate(video_url=ExpressionWrapper(F('url'),
-                                                                                   output_field=models.CharField()))
+            queryset = VideoContent.objects.filter(video_subscription__isnull=False
+                                                   ).filter(data_end__gte=timezone.now())
+            if queryset:
+                queryset = Video.objects.all().annotate(video_url=ExpressionWrapper(F('url'),
+                                                                                    output_field=models.CharField()))
+            else:
+                queryset = Video.objects.filter(Q(videocontent__user_id=self.request.user.id) &
+                                                Q(videocontent__data_end__gte=timezone.now())
+                                                ).annotate(video_url=ExpressionWrapper(F('url'),
+                                                                                    output_field=models.CharField()))
         else:
             queryset = None
         return queryset
