144e432fc72c9e3b0b1e243fafa9b649c6d56ac2
Vismmax <vismmax@gmail.com>
2021-06-08 01:05:00 +0300

    feat: delete mock

--- frontend/src/api/services/subscriptionService.ts
+++ frontend/src/api/services/subscriptionService.ts
@@ -1,8 +1,5 @@
 import webApi from '../webApiHelper';
 import { Subscription } from '../../common/interfaces/SubscriptionInterface';
-import { timeoutMock } from './timeoutMock';
-import { subscriptionMock } from './subscriptionMock';
-import { subscriptionAllMock } from './subscriptionAllMock';
 import { Endpoints } from '../../common/enums/EndpointsEnum';
 import { FetchError } from '../../common/interfaces/FetchErrorInterface';
 
@@ -22,31 +19,26 @@ interface SubscriptionAllResponse {
 
 export const getAllSubscriptions = async (): Promise<SubscriptionAllResponse> => {
   return await webApi.get(`${endpoint}/`);
-  // return await timeoutMock(subscriptionAllMock);
 };
 
 export const getSubscription = async (
   id: number,
 ): Promise<SubscriptionResponse> => {
-  // return await webApi.get(`${endpoint}/${id}`);
-  return await timeoutMock(subscriptionMock);
+  return await webApi.get(`${endpoint}/${id}`);
 };
 
 export const addSubscription = async (
   request: SubscriptionRequest,
 ): Promise<SubscriptionResponse> => {
-  // return await webApi.post(endpoint, request);
-  return await timeoutMock(subscriptionMock);
+  return await webApi.post(endpoint, request);
 };
 
 export const updateSubscription = async (
   request: Subscription,
 ): Promise<SubscriptionResponse> => {
-  // return await webApi.put(`${endpoint}/${request.id}`, request);
-  return await timeoutMock(subscriptionMock);
+  return await webApi.put(`${endpoint}/${request.id}`, request);
 };
 
 export const deleteSubscription = async (id: number): Promise<void> => {
-  // return await webApi.delete(`${endpoint}/${id}`);
-  return await timeoutMock(null);
+  return await webApi.delete(`${endpoint}/${id}`);
 };
