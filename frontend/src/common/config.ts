3c7aab947fdc0e59e6ac2964c80f94a3a464a8b1
Vismmax <vismmax@gmail.com>
2021-07-07 15:48:49 +0300

    feat: update .env

--- frontend/src/common/config.ts
+++ frontend/src/common/config.ts
@@ -1,5 +1,15 @@
 export const apiUrl = process.env.REACT_APP_API_URL;
-export const merchantId = process.env.REACT_APP_MERCHANT_ID;
-export const merchantUrl = process.env.REACT_APP_MERCHANT_URL;
 
-export default { apiUrl, merchantId, merchantUrl };
+export const fondyMerchantId = process.env.REACT_APP_FONDY_MERCHANT_ID;
+export const fondyMerchantUrl = process.env.REACT_APP_FONDY_MERCHANT_URL;
+
+export const liqpayPublicKey = process.env.REACT_APP_LIQPAY_PUBLIC_KEY;
+export const liqpayPrivateKey = process.env.REACT_APP_LIQPAY_PRIVATE_KEY;
+
+export default {
+  apiUrl,
+  fondyMerchantId,
+  fondyMerchantUrl,
+  liqpayPublicKey,
+  liqpayPrivateKey,
+};
