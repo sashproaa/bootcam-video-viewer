7b1fd7d2d6925e1c4a174c7269095e9ccf9526d7
znnznn <znnznn@ukr.net>
2021-07-29 23:28:08 +0300

    создал файли по работе с google cloud

---
+++ backend/src/easyviewer/google_cloud_URL.py
@@ -0,0 +1,34 @@
+import datetime
+import os
+
+from google.cloud import storage
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\znnfo\Downloads\liquid-idiom-321118-87b74f56a8d8.json"
+
+
+def generate_download_signed_url_v4(bucket_name, blob_name):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    """
+    # bucket_name = 'your-bucket-name'
+    # blob_name = 'your-object-name'
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=15),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    print("Generated GET signed URL:")
+    print(url)
+    print("You can use this URL with any user agent, for example:")
+    print("curl '{}'".format(url))
+    return url
