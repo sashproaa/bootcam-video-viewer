e605316d6161db6c7708d34ece988d4de12e3b08
znnznn <znnznn@ukr.net>
2021-09-30 23:45:07 +0300

    переопределил метод reset comfirm сериализатора не работает base метод не декодируеся uid

--- backend/src/easyviewer/serializers.py
+++ backend/src/easyviewer/serializers.py
@@ -1,12 +1,16 @@
 from collections import OrderedDict
 
-from dj_rest_auth.serializers import LoginSerializer, UserDetailsSerializer
+from dj_rest_auth.serializers import LoginSerializer, UserDetailsSerializer, PasswordResetConfirmSerializer
+from django.contrib.auth.tokens import default_token_generator
 from django.db import transaction
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework.exceptions import ValidationError
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
-
+from django.contrib.auth import get_user_model
 from .models import *
 
 
@@ -256,3 +260,27 @@ class VideoPagination(LimitOffsetPagination):
             ('previous', self.get_previous_link()),
             ('results', data)
         ]))
+
+
+class CustomResetSerializer(PasswordResetConfirmSerializer):
+
+    def validate(self, attrs):
+        self._errors = {}
+        # Decode the uidb64 to uid to get User object
+        try:
+            uid = force_str(urlsafe_base64_decode(attrs['uid']))
+            self.user = get_user_model()._default_manager.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+            raise ValidationError({'uid': ['Invalid value']})
+
+        if not default_token_generator.check_token(self.user, attrs['token']):
+            raise ValidationError({'token': ['Invalid value']})
+        self.custom_validation(attrs)
+        # Construct SetPasswordForm instance
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs
+        )
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(self.set_password_form.errors)
+
+        return attrs
